21c92f2eb61d010e60da195581750b498d1240c4
Priya Sundaresan <priya.sundaresan@berkeley.edu>
2021-08-22 22:10:20 -0700

    exp_learn_stiffness that works

--- pysim/exp_learn_stiffness.py
+++ pysim/exp_learn_stiffness.py
@@ -28,8 +28,6 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
-handles = [25,30,54,60,62] # four corners + middle vertex
-
 if len(sys.argv)==1:
 	out_path = 'default_out'
 else:
@@ -50,8 +48,7 @@ class Net(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        # x = torch.clamp(x, min=-5, max=5)
+        x = torch.exp(self.fc3(x))
         return x
 
 with open('conf/rigidcloth/sysid/start.json','r') as f:
@@ -60,6 +57,7 @@ with open('conf/rigidcloth/sysid/start.json','r') as f:
 # PYTORCH3D
 device = torch.device("cuda:0")
 ref_verts, ref_faces, _ = load_obj("meshes/rigidcloth/sysid/ref_pinned_less_stiff.obj")
+#ref_verts, ref_faces, _ = load_obj("meshes/rigidcloth/sysid/ref_pinned_more_stiff.obj")
 ref_faces_idx = ref_faces.verts_idx.to(device)
 ref_verts = ref_verts.to(device)
 ref_mesh = Meshes(verts=[ref_verts], faces=[ref_faces_idx])
@@ -76,7 +74,7 @@ spf = config['frame_steps']
 scalev=1
 
 def reset_sim(sim, epoch):
-	if epoch % 5==0:
+	if epoch % 1==0:
 		arcsim.init_physics(out_path+'/conf.json', out_path+'/out%d'%epoch,False)
 	else:
 		arcsim.init_physics(out_path+'/conf.json',out_path+'/out',False)
@@ -93,72 +91,76 @@ def plot_pointcloud(points, title=""):
     ax.view_init(190, 30)
     plt.show()
 
-def get_loss(sim):
-    #loss = 0
-    #node_number = ref_verts.shape[0]
-    #for i in range(node_number):
-    #    loss += torch.norm(ref_verts[i]-(sim.cloths[0].mesh.nodes[i].x.to(device)))**2
-    #loss /= node_number
+def get_loss(sim, param_g):
+    reg  = torch.norm(param_g, p=2)*0.001
+    loss = 0
+    node_number = ref_verts.shape[0]
+    for i in range(node_number):
+        loss += torch.norm(ref_verts[i]-(sim.cloths[0].mesh.nodes[i].x.to(device)))**2
+    loss /= node_number
 
-    verts = torch.stack([v.node.x for v in sim.cloths[0].mesh.verts]).float().to(device)
-    faces = torch.Tensor([[vert.index for vert in f.v] for f in sim.cloths[0].mesh.faces]).to(device)
+    #verts = torch.stack([v.node.x for v in sim.cloths[0].mesh.verts]).float().to(device)
+    #faces = torch.Tensor([[vert.index for vert in f.v] for f in sim.cloths[0].mesh.faces]).to(device)
 
-    curr_mesh = Meshes(verts=[verts], faces=[faces])
+    #curr_mesh = Meshes(verts=[verts], faces=[faces])
 
-    sample_trg = sample_points_from_meshes(ref_mesh, 1000)
-    sample_src = sample_points_from_meshes(curr_mesh, 1000)
+    #sample_trg = sample_points_from_meshes(ref_mesh, 1000)
+    #sample_src = sample_points_from_meshes(curr_mesh, 1000)
 
-    loss_chamfer, _ = chamfer_distance(sample_trg, sample_src)
+    #loss_chamfer, _ = chamfer_distance(sample_trg, sample_src)
 
-    loss = loss_chamfer
-    return loss
+    #loss = loss_chamfer
+    return loss + reg
 
 def run_sim(steps, sim, net):
     net_input = torch.Tensor([steps]).to(device).double()
     net_output = net(net_input)
-    #print(net_output)
-    net_output_processed = torch.clamp(net_output, min=1e-2, max=1e3)
+    print(net_output)
+
+    #net_output_processed = torch.clamp(net_output, min=1e-2, max=1e3)
+    net_output_processed = net_output
 
     orig = sim.cloths[0].materials[0].stretching
     sim.cloths[0].materials[0].stretching = orig*net_output_processed.cpu()
 
     for step in range(steps):
-        print(step)
         arcsim.sim_step()
     
-    loss = get_loss(sim)
+    loss = get_loss(sim, net_output_processed)
     
     return loss
 
 def do_train(cur_step,optimizer,sim,net):
     epoch = 0
-    while epoch < 6:
-
-    	steps = 40
-
-    	reset_sim(sim, epoch)
-    
-    	st = time.time()
-    	loss = run_sim(steps, sim, net)
-    	en0 = time.time()
-    	
-    	optimizer.zero_grad()
-    
-    	loss.backward()
-    
-    	en1 = time.time()
-    	print("=======================================")
-    	print('epoch {}: loss={}\n'.format(epoch, loss.data))
-    
-    	print('forward tim = {}'.format(en0-st))
-    	print('backward time = {}'.format(en1-en0))
-    
-    	if epoch % 5 == 0:
-    		torch.save(net.state_dict(), torch_model_path)
-    
-    	optimizer.step()
-    	epoch = epoch + 1
-    	# break
+    while True:
+        steps = 40
+        
+        reset_sim(sim, epoch)
+        
+        st = time.time()
+        loss = run_sim(steps, sim, net)
+        en0 = time.time()
+        
+        optimizer.zero_grad()
+        
+        loss.backward()
+        
+        if loss < 0.005:
+            break
+        
+        en1 = time.time()
+        print("=======================================")
+        print('epoch {}: loss={}\n'.format(epoch, loss.data))
+        
+        print('forward tim = {}'.format(en0-st))
+        print('backward time = {}'.format(en1-en0))
+        
+        if epoch % 5 == 0:
+        	torch.save(net.state_dict(), torch_model_path)
+        
+        optimizer.step()
+        epoch = epoch + 1
+# break
 
 with open(out_path+('/log%s.txt'%timestamp),'w',buffering=1) as f:
     tot_step = 1
@@ -169,7 +171,7 @@ with open(out_path+('/log%s.txt'%timestamp),'w',buffering=1) as f:
     	net.load_state_dict(torch.load(torch_model_path))
     	print("load: %s\n success" % torch_model_path)
     
-    lr = 0.1
+    lr = 0.001
     momentum = 0.9
     f.write('lr={} momentum={}\n'.format(lr,momentum))
     optimizer = torch.optim.Adam(net.parameters(),lr=lr)
